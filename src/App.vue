a241f216947f874fae0374fd059a8d20d59b3d92
Johannes Thies <thies@soronsystems.com>
2026-06-15 10:28:51 +0200

    feat: include staging environment in page visit tracking logic

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import { useActiveWorkout } from "./composables/useActiveWorkout";
 const { activeWorkout } = useActiveWorkout();
 
 onMounted(() => {
-  if (['development', 'production'].includes(import.meta.env.MODE)) {
+  if (['development', 'staging', 'production'].includes(import.meta.env.MODE)) {
     const baseUrl = import.meta.env.VITE_API_BASE_URL;
     if (baseUrl) {
       fetch(`${baseUrl}/page-visits`, {
